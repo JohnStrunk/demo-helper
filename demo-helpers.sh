30cfd57651eb03f847610eb06abfc3588efdb1d6
John Strunk <jstrunk@redhat.com>
2021-08-11 10:05:08 -0400

    hereCmd
    Signed-off-by: John Strunk <jstrunk@redhat.com>

--- demo-helpers.sh
+++ demo-helpers.sh
@@ -1,9 +1,11 @@
 #! /bin/bash
 # shellcheck disable=SC2034
 
+
 ##################################################
 ## Color definitions & terminal manipulation
 ##################################################
+
 # See: http://mywiki.wooledge.org/BashFAQ/037
 fg_black="$(tput setaf 0)"
 fg_red="$(tput setaf 1)"
@@ -30,6 +32,28 @@ blink="$(tput blink)"
 standout="$(tput smso)"
 italic="$(tput sitm)"
 reset="$(tput sgr0)$(tput cnorm)"
+
+
+
+##################################################
+## Customizable settings
+##################################################
+
+# Typing characteristics for commands
+TYPING_DELAY_MS=100   # inter-char delay
+TYPING_JITTER_MS=150  # inter-char jitter
+
+# The styles for printing to the terminal
+STYLE_HEADING="${bold}${fg_red}"
+STYLE_MESSAGE="${bold}${fg_green}"
+STYLE_COMMAND="${bold}${fg_yellow}"
+STYLE_PAUSE="${bold}${fg_white}"
+
+
+
+##################################################
+## Implementation
+##################################################
 
 # Return terminal to normal on exit
 function resetterm {
@@ -70,22 +94,21 @@ function demonstrateCapabilities {
 ##################################################
 ## Helper functions
 ##################################################
-TYPING_DELAY_MS=130
-TYPING_JITTER_MS=100
 function typeit {
   string="$*"
   for (( idx=0; idx<${#string}; idx++ )); do
     echo -n "${string:$idx:1}"
     if [[ TYPING_DELAY_MS -gt 0 ]]; then
-      jitter=$(( (RANDOM % 10 - 5) * TYPING_JITTER_MS / 5 ))
+      jitter=$(( (RANDOM % 100 - 50) * TYPING_JITTER_MS / 50 ))
       delay=$(printf %.3f\\n "$((TYPING_DELAY_MS + jitter))e-3")
-      sleep "$delay"
+      sleep "$delay" > /dev/null 2>&1
     fi
   done
 }
 
+# Like cmd, but don't actually execute the command
 function showCmd {
-  echo -n "${bold}${fg_yellow}\$ "
+  echo -n "${STYLE_COMMAND}\$ "
   typeit "$*"
   echo "${reset}"
 }
@@ -98,7 +121,7 @@ function cmd {
 
 # Print a message: print <message>...
 function print {
-  echo "${bold}${fg_green}$*${reset}"
+  echo "${STYLE_MESSAGE}$*${reset}"
 }
 
 # Wait for a keypress: pressAnyKey [<message>...]
@@ -107,13 +130,13 @@ function pressAnyKey {
   if [[ -z $text ]]; then
     text="Press any key to continue..."
   fi
-  read -N1 -rs -p "${bold}${fg_white}$text${reset}${cursor_hide}"
+  read -N1 -rs -p "${STYLE_PAUSE}$text${reset}${cursor_hide}"
   echo "${reset}"
 }
 
 # Print a heading: heading <message>...
 function heading {
-  echo "${bold}${fg_red}$*${reset}"
+  echo "${STYLE_HEADING}$*${reset}"
 }
 
 # Auto-incrementing demo steps: step <message>...
@@ -122,3 +145,46 @@ function step {
   STEP_INDEX=$(( STEP_INDEX + 1 ))
   print "Step ${STEP_INDEX}: $*"
 }
+
+# Run a command that uses a HEREDOC
+# hereCmd <TAG> <HEREVAR> <command>...
+function hereCmd {
+  tag="$1"; shift
+  here="$1"; shift
+  thecmd=("$@")
+  thecmd+=("<<" "$tag")
+  showCmd "${thecmd[@]}"
+  sleep 2
+  echo "$here"
+  sleep 1
+  echo -n "$STYLE_COMMAND"
+  typeit "$tag"
+  echo "${reset}"
+  # shellcheck disable=SC2048
+  $* <<< "$here"
+}
+
+if [[ $(basename "$0") == "demo-helpers.sh" ]]; then
+  demonstrateCapabilities
+
+  heading "Headings look like this via heading"
+  print "You can print simple messages via print"
+  showCmd "Commands are run via cmd or just printed via showCmd"
+  step "the step command"
+  step "provides auto-numbered steps"
+  step "hereCmd can be used to run commands using heredoc"
+  CONTENTS=$(cat - <<THETAG
+apiVersion: scribe.backube/v1alpha1
+kind: ReplicationDestination
+metadata:
+  name: foo
+spec:
+  rsync:
+    serviceType: LoadBalancer
+    copyMethod: Snapshot
+    capacity: 700Gi
+    accessModes: [ReadWriteOnce]
+THETAG
+)
+  hereCmd "EOF" "$CONTENTS" "cat -"
+fi
